13e1dcd7aebe4623684280e6429d0d9090b96ba9
Eric <ericmuthuipatch22@gmail.com>
2023-09-08 09:43:54 +0300

    Completed Assignment

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python3
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import create_engine
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
 
-from models import Company, Dev
+Base = declarative_base(metadata=metadata)
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///freebies.db')
-    import ipdb; ipdb.set_trace()
+class Company(Base):
+    __tablename__ = 'companies'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    founding_year = Column(Integer())
+
+    freebies = relationship('Freebie', back_populates='company')
+
+
+    def __repr__(self):
+        return f'<Company {self.name}>'
+
+class Dev(Base):
+    __tablename__ = 'devs'
+
+    id = Column(Integer(), primary_key=True)
+    name= Column(String())
+
+    freebies = relationship('Freebie', back_populates='dev')
+
+    def __repr__(self):
+        return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey('companies.id'))
+
+    # Define relationships
+    dev = relationship('Dev', back_populates='freebies')
+    company = relationship('Company', back_populates='freebies')
